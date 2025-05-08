5765bbeb65dc29da7a017946eb34558defb4c711
pox <oxioxxxx@gmail.com>
2025-05-08 17:24:41 +0200

    update: for dotconf module exec files

---
+++ dotconf.fish
@@ -0,0 +1,21 @@
+#! /run/current-system/sw/bin/fish
+
+set one argv[0]
+
+set osr /etc/os-release
+set osname $(uname -s)
+
+if test -f $osr
+    set versionname $(grep '^NAME=' /etc/os-release | sed -E 's/^NAME="?(.*?)"?$/\1/')
+else
+    logs "$red Error$zero: files not found in this os status: $status !!!"
+end
+
+# running
+if test $versionname = NixOS
+    source $PWD/bin/nix-install.fish
+else
+    source $PWD/bin/install.sh
+end
+
+set -e osname one versionname
